8c468b7901a2395838bd85b73509f34c997cb731
Robert Rees <shudderfix@gmail.com>
2023-07-22 14:01:42 +0100

    Displays the total and the individual values in a single line

--- dice/src/main.v
+++ dice/src/main.v
@@ -1,15 +1,14 @@
 module main
 
+import arrays
 import os
 import rand
 
 fn main() {
 	if os.args.len != 1 + 3 {
-		println("Usage: dice x#ydz")
+		println("Usage: dice <number of rolls> <number of dice> <dice size>")
 		exit(1)
 	}
-
-	println(os.args[1])
 
 	set_size := os.args[1].int()
 
@@ -17,11 +16,19 @@ fn main() {
 
 	dice_size := 1 + os.args[3].int()
 
-	for _ in 0 .. set_size {
+	for i in 0 .. set_size {
+		println("Set ${i + 1}")
+		print("Roll: ")
+		mut rolls := []int{}
 		for _ in 0 .. pool_size {
 			a_roll := roll(dice_size)
-			println('Roll: ${a_roll}')
+			rolls << a_roll
 		}
+
+		total := arrays.sum(rolls) or { panic(err) }
+		roll_list := arrays.join_to_string(rolls, ', ', fn(i int) string { return i.str() })
+
+		println("(${total}) ${roll_list}")
 	}
 }
 
